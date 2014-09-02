200e3f004cee81aabeed4717f8fa26afae23d6e8
Deepika <mehtadeepika1313@gmail.com>
2014-09-01 23:36:27 -0700

    First Commit of TestMavenTestNG

---
+++ TestMavenTestNG/src/test/java/com/cisco/BingTest.java
@@ -0,0 +1,17 @@
+package com.cisco;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+	public class BingTest 
+	{
+		private WebDriver driver;
+		@Test
+		public void verifySearch() 
+		{
+			driver = new FirefoxDriver();
+			driver.get("http://www.bing.com/");
+			driver.quit();
+		}
+	}
---
+++ TestMavenTestNG/src/test/java/com/cisco/YahooTest.java
@@ -0,0 +1,17 @@
+package com.cisco;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+	public class YahooTest 
+	{
+		private WebDriver driver;
+		@Test
+		public void verifySearch() 
+		{
+			driver = new FirefoxDriver();
+			driver.get("http://www.google.com/");
+			driver.quit();
+		}
+	}
+
